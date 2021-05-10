ed414559be1cbabb0cf1e8e2d67fc85d32016b47
santosh-kumar-metikoti <santosh.metikoti@gmail.com>
2021-05-10 23:13:39 +0530

    changed arrayToObject

--- arrayToObject.js
+++ arrayToObject.js
@@ -3,7 +3,7 @@ function ArraytoObject(array) {
     return array.map(person => Object.assign(...person.map(([k, v]) => ({ [k]: v }))));
   }
   
-  var array = [[["name",'David'], ["skills",['HTM', 'CSS', 'JS', 'React']], ["scores",[98, 85, 90, 95]]]]
+  var array = [[["name",'David'], ["skills",['HTM', 'JS', 'CSS', 'React']], ["scores",[98, 85, 90, 95]]]]
   
   console.log(ArraytoObject(array));
    console.table(array);
