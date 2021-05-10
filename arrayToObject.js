973af1d6ec408c6975278637f96394ec857fee5e
santosh-kumar-metikoti <santosh.metikoti@gmail.com>
2021-05-10 23:12:09 +0530

    array to object

---
+++ arrayToObject.js
@@ -0,0 +1,9 @@
+function ArraytoObject(array) {
+
+    return array.map(person => Object.assign(...person.map(([k, v]) => ({ [k]: v }))));
+  }
+  
+  var array = [[["name",'David'], ["skills",['HTM', 'CSS', 'JS', 'React']], ["scores",[98, 85, 90, 95]]]]
+  
+  console.log(ArraytoObject(array));
+   console.table(array);
